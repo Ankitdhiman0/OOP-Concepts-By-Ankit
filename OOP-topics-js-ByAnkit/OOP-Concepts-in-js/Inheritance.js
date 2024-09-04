56ce8cead601092950854e39e9d98a77c728de86
Ankit Dhiman <136943956+Ankitdhiman0@users.noreply.github.com>
2024-09-04 12:25:03 +0530

    Update Inheritance.js

--- OOP-topics-js-ByAnkit/OOP-Concepts-in-js/Inheritance.js
+++ OOP-topics-js-ByAnkit/OOP-Concepts-in-js/Inheritance.js
@@ -2,6 +2,7 @@
 
 // The class that is inherited is known as parent class.
 // The class which inherits is known as child class.
+// The extends keyword is used to inherit the class. 
 
 class parent {
   constructor(A, B) {
